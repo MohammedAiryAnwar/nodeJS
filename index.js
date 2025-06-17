5a399d0969797bd40417b1d17dd285951eae7388
MohammedAiryAnwar <airyanwar@gmail.com>
2025-06-17 19:27:26 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// const arr = [1,2,3,4,5]
+// const newArr = [6,7]
+// console.log(arr.concat(newArr))
+
+// const arr = [1,2,3,4,5]
+// const newArr = [6,7]
+// const Carr = arr.concat(newArr)
+// console.log(Carr);
+
+// console.log(Carr.length);

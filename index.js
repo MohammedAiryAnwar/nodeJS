551357dfd6ec8ca7bf07aa6d38105be3567ec5f7
MohammedAiryAnwar <airyanwar@gmail.com>
2025-06-17 20:31:52 +0530

    JS Function

--- index.js
+++ index.js
@@ -8,3 +8,22 @@
 // console.log(Carr);
 
 // console.log(Carr.length);
+
+//++++++++++++++++function+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// function greet(name){
+//  return console.log(`Hello ${name}`);  
+// }
+// greet("Airy")
+
+//++++++++Arrow Function++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// const greet = (name)=>{
+//  return console.log( `Hello ${name}`); 
+// }
+// greet("Airy")
+
+//====================================================================================
+//const add = (a,b)=> a+b; 
+//const add = (a,b)=> (a+b);  
+//const add = (a,b)=>{return (a+b)}    //if use {} then must use"return"
+//console.log(add(5,4));
